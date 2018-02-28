fc63539dc9cae1f964a40acd289ee801287c696e
NadunKulatunge <nadunku.16@cse.mrt.ac.lk>
2018-02-28 23:56:38 +0530

    path change

--- signup.php
+++ signup.php
@@ -44,7 +44,7 @@ if(isset($_POST['btn-signup']))
 						Welcome to Coding Cage!<br/>
 						To complete your registration  please , just click following link<br/>
 						<br /><br />
-						<a href='http://localhost/x/verify.php?id=$id&code=$code'>Click HERE to Activate :)</a>
+						<a href='http://localhost/verify.php?id=$id&code=$code'>Click HERE to Activate :)</a>
 						<br /><br />
 						Thanks,";
 						
